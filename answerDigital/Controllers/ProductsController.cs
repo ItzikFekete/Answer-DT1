0579427d9568a0c54961a7b7165bc3df63033ff8
ItzikFekete <34029678+ItzikFekete@users.noreply.github.com>
2018-05-11 02:47:32 +0100

    Add project files.

---
+++ answerDigital/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using answerDigital.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace answerDigital.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+       {
+            new Product { Id = 1, FirstName = "", LastName = "", IsAuthorised = true, ColourId=1,ColourName="" }
+       };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ answerDigital/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace answerDigital.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAuthorised { get; set; }
+        public int ColourId { get; set; }
+        public string ColourName { get; set; }
+    }
+}
